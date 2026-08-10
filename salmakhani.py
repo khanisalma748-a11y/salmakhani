7e1686ece53c9703669028e18ccce90967b40c11
khanisalma748-a11y <khanisalma748@gmail.com>
2026-08-11 04:13:35 +0500

    Update salmakhani.py

--- salmakhani.py
+++ salmakhani.py
@@ -41,13 +41,18 @@ os.system('xdg-open https://youtu.be/Rx4eNsxjCbE?si=PEa1vc3uShrxG723')
 os.system('xdg-open https://youtu.be/Rx4eNsxjCbE?si=PEa1vc3uShrxG723')
 # --- GITHUB APPROVAL SYSTEM ---
 def arsalan_approval():
+   
     os.system('clear')
+  
     # Aapka unique key identifier
+   
     uuid_raw = str(os.getlogin()) + str(os.getuid())
+    
     key = hashlib.md5(uuid_raw.encode()).hexdigest().upper()[:12]
     
     # Yahan apni GitHub link lagayein jahan approved keys hongi
-   # Format: Raw text file on GitHub
+ 
+# Format: Raw text file on GitHub
  github_link = "https://github.com/khanisalma748-a11y/salmakhani.git/blob/main/APROVEL-78"
     
 print("\033[1;31m  _    ____ _____   _    _       _    _   _ ")
