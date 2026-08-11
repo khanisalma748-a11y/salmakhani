e7a67b64cd0a31d5fdeb6bff05da1192cc4473c0
khanisalma748-a11y <khanisalma748@gmail.com>
2026-08-11 05:21:41 +0500

    Update salmakhani.py

--- salmakhani.py
+++ salmakhani.py
@@ -488,9 +488,11 @@ def login_1(uid):
     'fb_api_req_friendly_name': 'authenticate',
     'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler',
     'api_key': '882a8490361d98702bf97a021ddc14d'
-            }
+
+
+                
             
-                headers = {
+         headers = {
         'User-Agent': 'Mozilla/5.0',
         'Accept-Encoding': 'gzip, deflate',
         'Connection': 'keep-alive'
