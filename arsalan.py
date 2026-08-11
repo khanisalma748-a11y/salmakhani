13cf6b2dd8b6be0c1863d6ce76c617ff412e7e3b
khanisalma748-a11y <khanisalma748@gmail.com>
2026-08-12 04:50:02 +0500

    Update arsalan.py

--- arsalan.py
+++ arsalan.py
@@ -239,63 +239,52 @@ def ____banner____():
 \033[0m""")
 
 
-def creationyear(uid):
+def  creationyear(uid):
     """
     Estimates the Facebook account creation year based on the UID.
-    """   
+    """
     if len(uid) == 10:
         if uid.startswith(('11529', '11503', '115')):
             return '2008'
+            
     if len(uid) == 15:
         if uid.startswith('1000000000'):
             return '2009'
-        if uid.startswith('100000000'):
+        elif uid.startswith(('1000001', '1000002', '1000003', '1000004', '1000005')):
             return '2009'
-        if uid.startswith('10000000'):
-            return '2009'
-        if uid.startswith(('1000000', '1000001', '1000002', '1000003', '1000004', '1000005')):
-            return '2009'
-        if uid.startswith(('1000006', '1000007', '1000008', '1000009')):
+        elif uid.startswith(('1000006', '1000007', '1000008', '1000009')):
             return '2010'
-        if uid.startswith('100001'):
+        elif uid.startswith('100001'):
             return '2010'
-        if uid.startswith(('100002', '100003')):
+        elif uid.startswith(('100002', '100003')):
             return '2011'
-        if uid.startswith('100004'):
+        elif uid.startswith('100004'):
             return '2012'
-        if uid.startswith(('100005', '100006')):
+        elif uid.startswith(('100005', '100006')):
             return '2013'
-        if uid.startswith(('100007', '100008')):
+        elif uid.startswith(('100007', '100008')):
             return '2014'
-        if uid.startswith('100009'):
+        elif uid.startswith('100009'):
             return '2015'
-        if uid.startswith('10001'):
+        elif uid.startswith('10001'):
             return '2016'
-        if uid.startswith('10002'):
+        elif uid.startswith('10002'):
             return '2017'
-        if uid.startswith('10003'):
+        elif uid.startswith('10003'):
             return '2018'
-        if uid.startswith('10004'):
+        elif uid.startswith('10004'):
             return '2019'
-        if uid.startswith('10005'):
+        elif uid.startswith('10005'):
             return '2020'
-        if uid.startswith('10006'):
+        elif uid.startswith('10006'):
             return '2021'
-        if uid.startswith('10009'):
-            return '2023'
-        if uid.startswith(('10007', '10008')):
+        elif uid.startswith(('10007', '10008')):
             return '2022'
-        return ''
-    elif len(uid) in (9, 10):
-        return '2008'
-    elif len(uid) == 8:
-        return '2007'
-    elif len(uid) == 7:
-        return '2006'
-    elif len(uid) == 14 and uid.startswith('61'):
-        return '2024'
-    else:
-        return ''
+        elif uid.startswith('10009'):
+            return '2023'
+            
+    return ''
+
 
 
 def clear():
